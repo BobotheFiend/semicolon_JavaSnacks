f7b329720fec6aae98125ed7f822aa79fac9cb48
Nnamdi <cavennnamdi@gmail.com>
2026-04-09 08:41:29 +0100

    ProductDisplay

--- Doughnuts/littleLoops/ProductDisplay.java
+++ Doughnuts/littleLoops/ProductDisplay.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 public class ProductDisplay{
     public static void main(String[] args){
         Scanner inputNumber = new Scanner(System.in);
-
+        
+        int result = 1;
         int i = 1;
-        int j = 2;
         while (i <= 10){
-        System.out.printf("%d %n", i*j);
-       i++;
-        j++;
-       
-        }
+        result = result * i;
+        System.out.printf("%d = %d%n", i, result);
+       i++;}
     }
 }
